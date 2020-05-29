68bd80fa8ad48a352273ffd48eaa5f16fd9a9619
TARCISIO NUNES <tarcnux@gmail.com>
2020-05-29 08:39:26 -0300

    Busca de dados na API do Github com Axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App(props) {
   const [ usuario, setUsuario ] = useState('');
-  return (    //Retornando um fragment 
+  function handlePesquisa() {
+    //console.log(usuario);
+    axios.get('https://api.github.com/users/tarcnux/repos').then(
+      response => console.log(response)
+    );
+  }
+  return (    //Retornando um fragment     
     <>             
       <p> { usuario } </p>
       <input placeholder="usuario" className="usuarioInput" 
-            onChange={ e => setUsuario(e.target.value)} value = { usuario } />
-      <button type="button">Pesquisar do uri</button>
+            onChange={ e => setUsuario(e.target.value)} value={ usuario } />
+      <button type="button" onClick={ handlePesquisa } >Pesquisar do uri</button>
     </>
   );
 }
