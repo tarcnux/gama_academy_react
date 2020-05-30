1d192c12866da012774fe4896577f579c885a89a
TARCISIO NUNES <tarcnux@gmail.com>
2020-05-30 11:37:24 -0300

    Armazenando os nomes dos repositórios no localStorage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,15 +8,24 @@ function Home(props) {
     //console.log(usuario);
     // usuários exemplos: ramosht, octocat
     axios.get(`https://api.github.com/users/${ usuario }/repos`).then(
-      response => console.log(response.data)
-    );
+      response => {
+        const repos = response.data;
+        const reposName = [];
+        repos.map((repo) => {
+          reposName.push(repo.name);
+        });
+        //console.log(reposName);
+        localStorage.setItem('reposName', JSON.stringify(reposName));       
+      });
   }
   return (    //Retornando um fragment     
     <S.Container>             
-      <p> { usuario } </p>
+      {/* <p> { usuario } </p> */}
       <S.Input placeholder="usuario" className="usuarioInput" 
             onChange={ e => setUsuario(e.target.value)} value={ usuario } />
-      <S.Button type="button" onClick={ handlePesquisa } >Pesquisar do uri</S.Button>
+      <S.Button type="button" onClick={ handlePesquisa } >
+        Pesquisar do uri
+      </S.Button>
     </S.Container>
   );
 }
